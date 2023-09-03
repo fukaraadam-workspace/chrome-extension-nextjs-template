a15e071d10bf86920374326db6cc671e9e5d2c28
Furkan Alan <furkanalan@hotmail.com>
2023-09-03 06:19:16 +0300

    added utility scripts folder, added renaming _next to next folder name as first script

---
+++ scripts/README.md
@@ -0,0 +1,5 @@
+# Utility Scripts
+
+This folder contains utility scripts to be used manually. Has its own package.json and typescript configuration with ts-node, so it can be used independently from main project.
+
+For scripts, use `npm run <script-name>` command.
---
+++ scripts/src/rename-next-folder.ts
@@ -0,0 +1,69 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+const outDirectory = '../out';
+const directoryToRename = outDirectory + '/_next';
+const newDirectoryName = outDirectory + '/next';
+
+// Rename the directory
+fs.rename(directoryToRename, newDirectoryName, (err) => {
+  if (err) {
+    console.error('--Error renaming directory--');
+    throw err;
+  } else {
+    console.log(`Directory ${directoryToRename} renamed to ${newDirectoryName}`);
+    
+    // Search and replace in HTML files
+    searchAndReplaceInFiles(outDirectory, /\/_next/g, '/next');
+  }
+});
+
+function searchAndReplaceInFiles(directory: string, searchPattern: string | RegExp, replaceText: string) {
+  fs.readdir(directory, (err, files) => {
+    if (err) {
+      console.error('--Error reading directory--');
+      throw err;
+    }
+
+    files.forEach((file) => {
+      const filePath = path.join(directory, file);
+
+      fs.stat(filePath, (statErr, stats) => {
+        if (statErr) {
+          console.error('--Error getting file stats--');
+          throw statErr;
+        }
+
+        if (stats.isDirectory()) {
+          // Recursively search and replace in subdirectories
+          searchAndReplaceInFiles(filePath, searchPattern, replaceText);
+        } else if (stats.isFile()) {
+          // Process HTML files
+          fs.readFile(filePath, 'utf8', (readErr, data) => {
+            if (readErr) {
+              console.error('--Error reading file--');
+              throw readErr;
+            }
+
+            let isFoundInFile = false;
+            const updatedData = data.replaceAll(searchPattern, function (_match) {
+              isFoundInFile = true;
+              return replaceText;
+            });
+
+            if(isFoundInFile) {
+              fs.writeFile(filePath, updatedData, 'utf8', (writeErr) => {
+                if (writeErr) {
+                  console.error('--Error writing file--');
+                  throw writeErr;
+                } else {
+                  console.log(`Updated file: ${filePath}`);
+                }
+              });
+            }
+          });
+        }
+      });
+    });
+  });
+}
