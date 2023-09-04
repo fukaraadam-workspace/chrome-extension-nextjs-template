67820ce517ac565d85450ddadbc2e01bf31c5207
Furkan Alan <furkanalan@hotmail.com>
2023-09-04 03:31:29 +0300

    moved manifest.json of extension, developed webpack

---
+++ packages/extension-specific/webpack.config.ts
@@ -0,0 +1,41 @@
+import webpack from 'webpack';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+import * as path from 'path';
+
+module.exports = {
+  entry: {
+    content: './src/content.ts',
+    background: './src/background.ts',
+  },
+  mode: 'production',
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  plugins: [
+    new CleanWebpackPlugin({ verbose: false }),
+    new webpack.ProgressPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'public/manifest.json',
+          to: path.join(__dirname, 'out'),
+          force: true,
+          transform: function (content, path) {
+            // generates the manifest file using the package.json informations
+            return Buffer.from(
+              JSON.stringify({
+                version: process.env.npm_package_version,
+                ...JSON.parse(content.toString()),
+              }),
+            );
+          },
+        },
+      ],
+    }),
+  ],
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'out'),
+  },
+};
