3625db36122ba64021ec15f35a1058eff5204dc5
Furkan Alan <furkanalan@hotmail.com>
2023-09-04 05:18:55 +0300

    developed extension background script, added listener, temporarily removed app-ui

--- packages/extension-specific/src/background.ts
+++ packages/extension-specific/src/background.ts
@@ -2,3 +2,18 @@ import { generate } from 'random-words';
 
 console.log('Background script loaded! Generating random words...');
 console.log(generate());
+
+function reddenPage() {
+  console.log('Extension Button Clicked! Running from page');
+  document.body.style.backgroundColor = 'red';
+}
+
+chrome.action.onClicked.addListener((tab) => {
+  if (!tab.url?.includes('chrome://') && tab.id) {
+    console.log('Extension Button Clicked! Running from extension');
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      func: reddenPage,
+    });
+  }
+});
