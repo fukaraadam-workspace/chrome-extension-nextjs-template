512f4549cfcd5e3b6051ed1ed307d9e03b942986
Furkan Alan <furkanalan@hotmail.com>
2023-09-07 23:57:05 +0300

    popup size adjustment

--- packages/app-ui/src/components/layout/layout.tsx
+++ packages/app-ui/src/components/layout/layout.tsx
@@ -2,9 +2,5 @@
 import LayoutClient from './layout-client';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="w-[400px] h-[600px]">
-      <LayoutClient>{children}</LayoutClient>
-    </div>
-  );
+  return <LayoutClient>{children}</LayoutClient>;
 }
