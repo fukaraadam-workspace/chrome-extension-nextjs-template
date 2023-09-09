8ae451e3d48ce4a4cccc3ed57fa642cf14e29c60
Furkan Alan <furkanalan@hotmail.com>
2023-09-10 00:09:31 +0300

    Added app title and description

--- packages/app-ui/src/components/layout/layout.tsx
+++ packages/app-ui/src/components/layout/layout.tsx
@@ -1,6 +1,18 @@
+import Head from 'next/head';
 // Components
 import LayoutClient from './layout-client';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <LayoutClient>{children}</LayoutClient>;
+  return (
+    <LayoutClient>
+      <Head>
+        <title>{process.env.NEXT_PUBLIC_APP_TITLE}</title>
+        <meta
+          name="description"
+          content={process.env.NEXT_PUBLIC_APP_DESCRIPTION}
+        />
+      </Head>
+      {children}
+    </LayoutClient>
+  );
 }
